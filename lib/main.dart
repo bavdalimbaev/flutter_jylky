e27a3c60a173ae5ef136cbadfb6b85add7a01981
ArianRin <rin.meizu@gmail.com>
2021-08-06 20:40:06 +0600

    first of first

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'homepage.dart';
+import 'package:flutter/services.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (Platform.isAndroid) {
+    await AndroidInAppWebViewController.setWebContentsDebuggingEnabled(true);
+  }
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+       statusBarColor: Colors.white,
+       systemNavigationBarColor: Color(0xff95652b),
+       statusBarIconBrightness: Brightness.dark,
+       systemNavigationBarIconBrightness:Brightness.light    
+    ));
+    return MaterialApp(
+      title: 'Жылкы KG - Ферма',
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
---
+++ lib/webview/jylky.dart
@@ -0,0 +1,159 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+// import 'package:full_webview/check_internet.dart';
+
+class WebExampleTwo extends StatefulWidget {
+  WebExampleTwo({Key? key}) : super(key: key);
+
+  @override
+  _WebExampleTwoState createState() => _WebExampleTwoState();
+}
+
+class _WebExampleTwoState extends State<WebExampleTwo> {
+  InAppWebViewController? _webViewController;
+  double progress = 0;
+  String url = '';
+  int checkInt = 0;
+
+  final GlobalKey webViewKey = GlobalKey();
+
+  InAppWebViewGroupOptions options = InAppWebViewGroupOptions(
+      crossPlatform: InAppWebViewOptions(
+        userAgent: 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Mobile Safari/537.36',
+        useShouldOverrideUrlLoading: true,
+        mediaPlaybackRequiresUserGesture: false,
+        javaScriptEnabled: true,
+        javaScriptCanOpenWindowsAutomatically: true,
+      ),
+      android: AndroidInAppWebViewOptions(
+        useHybridComposition: true,
+      ),
+      ios: IOSInAppWebViewOptions(
+        allowsInlineMediaPlayback: true,
+      ));
+
+  late PullToRefreshController pullToRefreshController;
+  final urlController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Future<int> a = CheckInternet().checkInternetConnection();
+    // a.then((value) {
+    //   if (value == 0) {
+    //     setState(() {
+    //       checkInt = 0;
+    //     });
+    //     print('Нет соединение к сети');
+    //     ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+    //       content: Text('Нет соединение к сети!'),
+    //     ));
+    //   } else {
+    //     setState(() {
+    //       checkInt = 1;
+    //     });
+    //     print('Интернет подключен');
+    //     ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+    //       content: Text('Интернет подключен'),
+    //     ));
+    //   }
+    // });
+
+
+
+
+
+    pullToRefreshController = PullToRefreshController(
+      options: PullToRefreshOptions(color: Colors.blue),
+      onRefresh: () async {
+        if (Platform.isAndroid) {
+          _webViewController?.reload();
+        } else if (Platform.isIOS) {
+          _webViewController?.loadUrl(
+              urlRequest: URLRequest(url: await _webViewController?.getUrl()));
+        }
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          child: Column(
+            children: [
+              progress < 1.0
+                  ? LinearProgressIndicator(
+                      value: progress,
+                      backgroundColor: Colors.white,
+                      valueColor:
+                          AlwaysStoppedAnimation<Color>(Colors.green[800]!),
+                    )
+                  : Center(),
+              Expanded(
+                child: InAppWebView(
+                  key: webViewKey,
+                  initialUrlRequest: URLRequest(
+                      url: Uri.parse("https://jylky.kg")),
+                  initialOptions: options,
+                  pullToRefreshController: pullToRefreshController,
+                  onWebViewCreated: (controller) {
+                    _webViewController = controller;
+                  },
+                  onLoadStart: (controller, url) {
+                    setState(() {
+                      this.url = url.toString();
+                      urlController.text = this.url;
+                    });
+                  },
+                  androidOnPermissionRequest:
+                      (controller, origin, resources) async {
+                    return PermissionRequestResponse(
+                        resources: resources,
+                        action: PermissionRequestResponseAction.GRANT);
+                  },
+                  onLoadStop: (controller, url) async {
+                    pullToRefreshController.endRefreshing();
+                    setState(() {
+                      this.url = url.toString();
+                      urlController.text = this.url;
+                    });
+                  },
+                  onLoadError: (controller, url, code, message) {
+                    pullToRefreshController.endRefreshing();
+                  },
+                  onProgressChanged: (controller, progress) {
+                    if (progress == 100) {
+                      pullToRefreshController.endRefreshing();
+                    }
+                    setState(() {
+                      this.progress = progress / 100;
+                      urlController.text = this.url;
+                    });
+                  },
+                  onUpdateVisitedHistory: (controller, url, androidIsReload) {
+                    setState(() {
+                      this.url = url.toString();
+                      urlController.text = this.url;
+                    });
+                  },
+                  onConsoleMessage: (controller, consoleMessage) {
+                    print(consoleMessage);
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
